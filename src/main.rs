f6cf9af7bd94031efa5f989c460fa159596199d7
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2023-04-27 18:33:09 -0300

    feat: users router boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use actix_web::{web, App, HttpServer};
+use routes::{health, users};
 use state::TweterooState;
 
 mod routes;
 mod state;
-
-use crate::routes::health::health;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .app_data(web::Data::new(TweterooState::new()))
-            .service(health)
+            .service(health::health)
+            .service(users::sign_up)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
---
+++ src/routes/users.rs
@@ -0,0 +1,24 @@
+use actix_web::{post, web, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+use std::ops::DerefMut;
+
+use crate::state;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SignUpReqBody {
+    username: String,
+    avatar: String,
+}
+
+#[post("/sign-up")]
+pub async fn sign_up(
+    req_body: web::Json<SignUpReqBody>,
+    state: web::Data<state::TweterooState>,
+) -> impl Responder {
+    println!("body {:?}", req_body);
+
+    let user = state::User::new(&req_body.username, &req_body.avatar);
+    state.add_user(user);
+
+    HttpResponse::Ok().body("Sign up!")
+}
